ee9e09ba68bd5a98fee474ae16e9112432ad0f4f
Micah Walter <micah@micahwalter.com>
2013-09-13 22:59:55 -0400

    adding a bunch of login stuff

--- download/urls.py
+++ download/urls.py
@@ -4,6 +4,10 @@ from download import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^about/$', views.about, name='about'),
 	url(r'^search/$', views.search, name='search'),
 	url(r'^thanks/$', views.thanks, name='thanks'),
+	
+	url(r'^login/$', views.login_page, name='login'),
+	url(r'^logout/$', views.logout_page, name='logout'),
 )
--- download/views.py
+++ download/views.py
@@ -1,6 +1,7 @@
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from download.models import Download
-import os
+import os, uuid
 import cooperhewitt.api.client
 import json
 
@@ -10,6 +11,10 @@ hostname = os.environ['CH_API_HOST']
 def index(request):
 	context = {}
 	return render(request, 'download/index.html', context)
+	
+def about(request):
+	context = {}
+	return render(request, 'download/about.html', context)
 
 def search(request):	
 	try:
@@ -23,8 +28,55 @@ def search(request):
 		return render(request, 'download/search.html', context)
 	
 def thanks(request):
+	try:
+		#email = request.POST['email']
+		#object_ids = request.POST['object_ids']
+		search_string = request.POST['search_string']
+	except (KeyError, Download.DoesNotExist):
+		return redirect('/')
+	else:
+		event_id = uuid.uuid4()
+		event_id = str(event_id)
+
+		event = {
+			'event_id': event_id,
+			'action': 'download_tool',
+			'search_string': search_string,
+			 #'object_ids': object_ids
+		}
+	
+		api = cooperhewitt.api.client.OAuth2(access_token, hostname=hostname)
+		method = 'millerfox.eventserver.registerEvents'
+		args = { 'events': events }
+		
+		rsp = api.call(method, **args)
+	
+		context = {'rsp':rsp}
+		return render(request, 'download/thanks.html', context)	
+
+def login_page(request):
 	context = {}
-	return render(request, 'download/thanks.html', context)	
+	
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+	
+		user = authenticate(username=username, password=password)
+	
+		if user is not None:
+			if user.is_active:
+				login(request, user)
+				return render(request, 'download/index.html', context)
+		else:
+			return render(request, 'download/login.html', context)
+	
+	return render(request, 'download/login.html', context)
+
+def logout_page(request):
+	context = {}
+	logout(request)	
+	
+	return render(request, 'download/logout.html', context)
 
 def search_objects(query):
 	api = cooperhewitt.api.client.OAuth2(access_token, hostname=hostname)
@@ -34,4 +86,5 @@ def search_objects(query):
 	rsp = api.call(method, **args)
 	
 	return rsp
+	
 	
